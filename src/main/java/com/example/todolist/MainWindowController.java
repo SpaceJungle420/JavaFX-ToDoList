ecfff936087c0f63b876595a0d412a104ebd51ba
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-07-23 16:17:46 -0300

    setup sample ToDo List Application

--- src/main/java/com/example/todolist/MainWindowController.java
+++ src/main/java/com/example/todolist/MainWindowController.java
@@ -1,4 +1,32 @@
 package com.example.todolist;
 
+import com.example.todolist.datamodel.ToDoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainWindowController {
+    private List<ToDoItem> todoItems;
+
+    public void initialize() {
+        ToDoItem item1 = new ToDoItem("Mail birthday card", "Buy a 30th birthday card for jhon",
+                LocalDate.of(2016, Month.APRIL, 25));
+        ToDoItem item2 = new ToDoItem("Doctor's Appointment", "See Dr. Smith at 123 Main Street. Bring paperwork",
+                LocalDate.of(2016, Month.MAY, 25));
+        ToDoItem item3 = new ToDoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday 22 of April",
+                LocalDate.of(2016, Month.APRIL, 22));
+        ToDoItem item4 = new ToDoItem("Pickup doug at the train station", "Doug's arriving on March 23 on the 5:00 train",
+                LocalDate.of(2016, Month.MARCH, 23));
+        ToDoItem item5 = new ToDoItem("Pickup dry cleaning", "The clothes should be ready by wednesday",
+                LocalDate.of(2016, Month.APRIL, 20));
+
+        todoItems = new ArrayList<ToDoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+    }
 }
